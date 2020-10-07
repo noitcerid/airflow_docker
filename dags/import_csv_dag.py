e56adf9292cedf9b86a0d35a8c500f15271e21b0
Chris Potter <christhepcguy@gmail.com>
2020-10-07 06:34:12 -0400

    Updated dag to simple working example

--- dags/import_csv_dag.py
+++ dags/import_csv_dag.py
@@ -1,17 +1,48 @@
-#import_csv_dag.py
+"""
+    Airflow w/ Python Operator
+"""
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 import csv
 import os
 import shutil
 
+
 def copy_file(source_file, destination_directory='complete'):
     """ This will copy a file from import directory to complete """
-    current_directory = os.getcwd()
-    shutil.move(current_directory + '/' + source_file, current_directory + '/' + source_file)
+    current_directory = '/usr/local/airflow/data/'
+    shutil.move(current_directory + source_file, current_directory + destination_directory + '/' + source_file)
 
 
 def import_file(name):
-    """This will import the specified CSV file from the current working directory to...?"""
+    """This will import the specified CSV file from the current working directory to stage db"""
+    print(name)
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['christopher.s.potter@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'copy_file',
+    default_args=default_args,
+    description='Move files from drop directory to complete folder',
+    schedule_interval=timedelta(hours=1)
+)
+
+task1 = PythonOperator(
+    task_id='copy_file',
+    python_callable=copy_file,
+    op_kwargs={'source_file': 'sales_data.csv'},
+    dag=dag
+)
